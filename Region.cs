789f0223a1398e760045f95588a7a4fc49e4b722
Bolukan <github@bolukan.nl>
2014-03-08 12:33:38 +0100

    Init class
    Region class

---
+++ Region.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TweakBot
+{
+    class Region
+    {
+        private int id;
+        private List<Region> neighbours;
+        private SuperRegion superRegion;
+        private int armies;
+        private String playerName;
+        
+        /// <summary>
+        /// Initialise Region
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <param name="superRegion">SuperRegion</param>
+        public Region(int id, SuperRegion superRegion)
+        {
+            this.id = id;
+            this.superRegion = superRegion;
+            this.neighbours = new List<Region>();
+            this.armies = 0;
+            this.playerName = "Unknown";
+        }
+
+        public
+
+
+    }
+}
