80e08a2c829ab5e4fd052bc02053fb545c6a1c90
Bolukan <github@bolukan.nl>
2014-03-08 14:06:15 +0100

    some setup code

--- BotState.cs
+++ BotState.cs
@@ -18,11 +18,43 @@ namespace TweakBot
         private List<Region> regions;
         private List<SuperRegion> superRegions;
 
+        public void setup_map(String[] parts)
+        {
+            switch (parts[1])
+            {
+                case "super_regions":
+                    // TODO: read and process super_regions
+                    break;
+                case "regions":
+                    // TODO: read and process regions
+                    break;
+                case "neighbours":
+                    // TODO: read and process neighbours
+                    break;
+                default:
+                    // EXCEPTION
+                    break;
+            }
+        }
 
+        public void settings(String[] parts)
+        {
+            switch (parts[1])
+            {
+                case "your_bot":
+                    // TODO: read and process super_regions
+                    break;
+                case "opponent_bot":
+                    // TODO: read and process regions
+                    break;
+                case "starting_armies":
+                    // TODO: read and process neighbours
+                    break;
+                default:
+                    // EXCEPTION
+                    break;
+            }
+        }
 
-
-    }
-
-
-
-}
+    } // class
+} // nameSpace
