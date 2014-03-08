8c41a873cfa5bac98bd958c25143b62a604f3176
Bolukan <github@bolukan.nl>
2014-03-08 12:55:31 +0100

    Init files

---
+++ BotParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TweakBot
+{
+    class BotParser
+    {
+        public String in = Console.
+
+        public void run()
+        {
+        }while 
+    }
+}
---
+++ BotState.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TweakBot
+{
+    /// <summary>
+    /// Contains data of the world
+    /// </summary>
+    
+    class BotState
+    {
+        private List<Region> regions;
+        private List<SuperRegion> superRegions;
+
+
+
+
+    }
+
+
+
+}
