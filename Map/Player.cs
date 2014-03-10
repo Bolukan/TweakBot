b6fac26ce670e4438422a0e0a28168a61cb5de34
Bolukan <github@bolukan.nl>
2014-03-10 22:07:50 +0100

    New Player class

---
+++ Map/Player.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace TweakBot
+{
+    enum Players : int { Unknown, Neutral, Me, Other }
+
+    class Player
+    {
+        static String[] playerName = new String[4];
+
+        static Player()
+        {
+            playerName[0] = "Unknown";
+            playerName[1] = "neutral";
+            playerName[2] = "me";
+            playerName[3] = "other";
+        }
+    
+        static public void SetMyName(String myName)
+        {
+            playerName[2] = myName;
+        }
+        
+        static public void SetOtherName(String otherName)
+        {
+            playerName[3] = otherName;
+        }
+
+        static public int Name(String name)
+        {
+            if (playerName[2] == name) return 2;
+            if (playerName[3] == name) return 3;
+            if (playerName[1] == name) return 1;
+            return 0;
+        }
+
+        static public String Name(int name)
+        {
+            return playerName[name];
+        }
+    }
+}
