3ab68ee1813f59b4f1286964a4347bc5380c3ac8
Bolukan <github@bolukan.nl>
2014-03-14 10:02:25 +0100

    Added Go objects AttackTransfer en PlaceArmies

---
+++ Go/AttackTransfer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace TweakBot
+{
+    public class AttackTransfer
+    {
+        public int Armies { get; set; }
+        public Region SourceRegion { get; set; }
+        public Region TargetRegion { get; set; }
+    
+        public AttackTransfer(int armies, Region sourceRegion, Region targetRegion)
+        {
+            Armies = armies;
+            SourceRegion = sourceRegion;
+            TargetRegion = targetRegion;
+        }
+
+        public String ToString()
+        {
+            return String.Format("{0} attack/transfer {1} {2} {3}", PLAYER.NameMe, SourceRegion.Id, TargetRegion.Id, Armies);
+        }
+    
+    }
+
+}
+ 
---
+++ Go/PlaceArmies.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace TweakBot
+{
+    public class PlaceArmies
+    {
+        public int Armies { get; set; }
+        public Region Region { get; set; }
+
+        public PlaceArmies(int armies, Region region)
+        {
+            Armies = armies;
+            Region = region;
+        }
+
+        public String ToString()
+        {
+            return String.Format("{0} place_armies {1} {2}", PLAYER.NameMe, Region.Id, Armies);
+        }
+    }
+}
